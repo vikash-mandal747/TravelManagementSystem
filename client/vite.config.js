8a9c45da83f82f17fc32603237695acac3453bc2
vikash-mandal747 <vikashmandal747@gmail.com>
2025-07-19 16:22:52 +0530

    Configure vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
+  base: '/TravelManagementSystem/', // 🔁 Replace this with your repo name
 })
